3dac334641707c05ff30f7d0e69a600d08a7f91f
Sebastian Stohr <11292940-_superhero1@users.noreply.gitlab.com>
2022-09-01 14:15:17 +0200

    Release of v0.8

---
+++ content.js
@@ -0,0 +1,196 @@
+/*
+DO NOT REMOVE, KEEP REFERENCE TO THE AUTHOR.
+---
+MIT License
+
+Copyright (c) 2022 Breaking IT - Sebastian Stohr
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+async function getStatements() {
+    var myHeaders = new Headers();
+    myHeaders.append("Authorization", "Bearer " + localStorage['id_token']);
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = "{\"operationName\":\"transactions_view\",\"variables\":{\"offset\":0,\"limit\":null,\"from\":null,\"to\":null},\"query\":\"query transactions_view($offset: Int, $limit: Int, $from: timestamptz, $to: timestamptz) {\\n  transactions_view_aggregate(\\n    where: {_and: [{date: {_gte: $from}}, {date: {_lte: $to}}]}\\n  ) {\\n    aggregate {\\n      totalCount: count\\n      __typename\\n    }\\n    __typename\\n  }\\n  transactions_view(\\n    order_by: {date: desc}\\n    limit: $limit\\n    offset: $offset\\n    where: {_and: [{date: {_gte: $from}}, {date: {_lte: $to}}]}\\n  ) {\\n    id\\n    model\\n    user_id\\n    currency\\n    amount\\n    date\\n    type\\n    is_debit\\n    description\\n    __typename\\n  }\\n}\\n\"}";
+
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    };
+
+    return await fetch("https://hasura.plutus.it/v1alpha1/graphql", requestOptions)
+        .then(response => response.json())
+        .then(jsonResponse => { return jsonResponse.data.transactions_view; })
+        .catch(err => console.warn(err));
+}
+
+async function getRewards() {
+    var myHeaders = new Headers();
+    myHeaders.append("Authorization", "Bearer " + localStorage['id_token']);
+
+    var requestOptions = {
+        method: 'GET',
+        headers: myHeaders,
+        redirect: 'follow'
+    };
+
+    return await fetch("https://api.plutus.it/platform/transactions/pluton", requestOptions)
+        .then(response => response.json())
+        .then(jsonResponse => { return jsonResponse; })
+        .catch(err => console.warn(err));
+}
+
+async function getOrders() {
+    var myHeaders = new Headers();
+    myHeaders.append("Authorization", "Bearer " + localStorage['id_token']);
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = "{\"variables\":{},\"extensions\":{},\"operationName\":null,\"query\":\"query { crypto_orders_view(\\n    order_by: {created_at: desc}\\n) {\\n    id\\n    model\\n    wallet\\n    status\\n    crypto_amount\\n    crypto_currency\\n    fiat_amount\\n    fiat_currency\\n    created_at\\n    updated_at\\n    __typename\\n  }\\n}\\n\"}";
+
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    };
+
+    return await fetch("https://hasura.plutus.it/v1alpha1/graphql", requestOptions)
+        .then(response => response.json())
+        .then(jsonResponse => { return jsonResponse.data.crypto_orders_view; })
+        .catch(err => console.warn(err));
+}
+
+async function getWithdrawals() {
+    var myHeaders = new Headers();
+    myHeaders.append("Authorization", "Bearer " + localStorage['id_token']);
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = "{\"operationName\":\"withdrawals\",\"variables\":{},\"query\":\"query withdrawals($status: enum_pluton_withdraw_requests_status) {\\n  pluton_withdraw_requests(\\n    order_by: {created_at: desc}\\n    where: {status: {_eq: $status}}\\n  ) {\\n    id\\n    address\\n    amount\\n    status\\n    payout_destination_type\\n    created_at\\n    clear_junction_transfer {\\n      amount\\n      currency\\n      __typename\\n    }\\n    card_transfer {\\n      amount\\n      currency\\n      __typename\\n    }\\n    __typename\\n  }\\n}\\n\"}";
+
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    };
+
+    return await fetch("https://hasura.plutus.it/v1alpha1/graphql", requestOptions)
+        .then(response => response.json())
+        .then(jsonResponse => { return jsonResponse.data.pluton_withdraw_requests; })
+        .catch(err => console.warn(err));
+}
+
+async function getTransactions() {
+    var myHeaders = new Headers();
+    myHeaders.append("Authorization", "Bearer " + localStorage['id_token']);
+
+    var requestOptions = {
+        method: 'GET',
+        headers: myHeaders,
+        redirect: 'follow'
+    };
+
+    return await fetch("https://api.plutus.it/platform/transactions/contis", requestOptions)
+        .then(response => response.json())
+        .then(jsonResponse => { return jsonResponse; })
+        .catch(err => console.warn(err));
+}
+
+function fixStatements(json) {
+    json.forEach(function (record) {
+        switch (record.type) {
+            case "0":
+                record.type = "PENDING";
+                break;
+            case "5":
+                record.type = "DECLINED_POS_CHARGE";
+                break;
+            case "29":
+                record.type = "CARD_DEPOSIT";
+                break;
+            case "31":
+                record.type = "PURCHASE";
+                break;
+            case "45":
+                record.type = "REFUND";
+                break;
+        }
+    });
+    return json
+}
+
+function flattenJson(json) {
+    // Source: https://stackoverflow.com/a/61602592
+    const flatten = (obj, roots = [], sep = '.') => Object.keys(obj).reduce((memo, prop) => Object.assign({}, memo, Object.prototype.toString.call(obj[prop]) === '[object Object]' ? flatten(obj[prop], roots.concat([prop]), sep) : { [roots.concat([prop]).join(sep)]: obj[prop] }), {})
+    resultJson = []
+    json.forEach(function (record) {
+        var flatRecord = flatten(record);
+        resultJson.push(flatRecord);
+    });
+    return resultJson
+}
+
+function jsonToCsv(json) {
+    // Source: https://stackoverflow.com/a/31536517
+    var fields = Object.keys(json[0])
+    var replacer = function (key, value) { return value === null ? '' : value }
+    var csv = json.map(function (row) {
+        return fields.map(function (fieldName) {
+            return JSON.stringify(row[fieldName], replacer)
+        }).join(',')
+    })
+    csv.unshift(fields.join(','))
+    csv = csv.join('\r\n');
+    return csv
+}
+
+function downloadCSV(csv, filename) {
+    // Source: https://www.javatpoint.com/javascript-create-and-download-csv-file
+    var downloadLink = document.createElement('a');
+    downloadLink.href = 'data:text/csv;charset=utf-8,' + encodeURI(csv);
+    downloadLink.target = '_blank';
+
+    downloadLink.download = filename + '.csv';
+    downloadLink.click();
+}
+
+chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
+    const action = msg.action;
+    if (action === "statement") {
+        getStatements().then(response => fixStatements(response)).then(result => jsonToCsv(result)).then(csv => downloadCSV(csv, "account_statement")).then(sendResponse({ action: "statement", status: "done" }));
+    }
+    else if (action === "rewards") {
+        getRewards().then(response => flattenJson(response)).then(result => jsonToCsv(result)).then(csv => downloadCSV(csv, "pluton_rewards")).then(sendResponse({ action: "rewards", status: "done" }));
+    }
+    else if (action === "orders") {
+        getOrders().then(response => flattenJson(response)).then(result => jsonToCsv(result)).then(csv => downloadCSV(csv, "crypto_orders")).then(sendResponse({ action: "orders", status: "done" }));
+    }
+    else if (action === "withdrawals") {
+        getWithdrawals().then(response => flattenJson(response)).then(result => jsonToCsv(result)).then(csv => downloadCSV(csv, "withdrawals")).then(sendResponse({ action: "withdrawals", status: "done" }));
+    }
+    else if (action === "transactions") {
+        getTransactions().then(response => flattenJson(response)).then(result => jsonToCsv(result)).then(csv => downloadCSV(csv, "transactions")).then(sendResponse({ action: "transactions", status: "done" }));
+    } else {
+        sendResponse({ action: action, status: "done" })
+    }
+});
---
+++ popup.js
@@ -0,0 +1,94 @@
+/*
+DO NOT REMOVE, KEEP REFERENCE TO THE AUTHOR.
+---
+MIT License
+
+Copyright (c) 2022 Breaking IT - Sebastian Stohr
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+let lastAction, lastCaller;
+
+function sendMessage(caller, action) {
+    // prevent multiple executions
+    caller.disabled = true;
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        let tabId = tabs[0].id;
+        chrome.tabs.sendMessage(tabId, { action: action }, function (response) {
+            if (!chrome.runtime.lastError && typeof response !== 'undefined') {
+                if (response.action === action && response.status === "done") {
+                    // restore button after 2 seconds
+                    setTimeout(function () {
+                        caller.disabled = false;
+                    }, 2000);
+                }
+            } else {
+                // in case the runtime listener is not available store caller & action before reloading the tab
+                lastAction = action;
+                lastCaller = caller;
+                chrome.tabs.reload(tabId);
+            }
+        });
+    });
+}
+
+statement.addEventListener("click", async (event) => {
+    sendMessage(event.target, "statement");
+});
+orders.addEventListener("click", async (event) => {
+    sendMessage(event.target, "orders");
+});
+rewards.addEventListener("click", async (event) => {
+    sendMessage(event.target, "rewards");
+});
+withdrawals.addEventListener("click", async (event) => {
+    sendMessage(event.target, "withdrawals");
+});
+transactions.addEventListener("click", async (event) => {
+    sendMessage(event.target, "transactions");
+});
+
+// resume last action if reloaded
+chrome.tabs.onUpdated.addListener(
+    function (tabId, changeInfo, tab) {
+        if (changeInfo.status === "complete") {
+            if (lastCaller != null && lastAction != null) {
+                sendMessage(lastCaller, lastAction);
+            }
+        }
+    }
+);
+
+document.addEventListener("DOMContentLoaded", function () {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        let tabUrl = tabs[0].url;
+        if (!tabUrl.startsWith("https://dex.plutus.it/dashboard")) {
+            console.log("Clicked");
+            container.innerHTML = `
+            <div class="alert alert-danger d-flex align-items-center" role="alert">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi flex-shrink-0 me-2" role="img" aria-label="Danger:" viewBox="0 0 16 16" role="img">
+                    <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+                </svg>
+                <div>Please login to PlutusDex or change to the right tab.</div>
+            </div>
+            `
+        }
+    });
+});
